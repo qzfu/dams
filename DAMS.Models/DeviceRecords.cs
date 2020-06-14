3c9732625df2213ac38a0d743654467fd214d98a
付启忠 <qzfu@haiyi-info.com>
2020-06-14 12:35:20 +0800

    添加服务器类

---
+++ DAMS.Models/DeviceRecords.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAMS.Models
+{
+    public class DeviceRecords
+    {
+        public int Id { get; set; }
+        public int PID { get; set; }
+        public int VID { get; set; }
+        public string SerialNumber { get; set; }
+        public string readBuffer { get; set; }
+        public int IsCopyEnd { get; set; }
+    }
+}
