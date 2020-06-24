c42cbe4402cece3aa072b895638322c2a22e7541
qzpan <1963315159@qq.com>
2020-06-24 19:57:10 +0800

    增加监听时间

--- DAMS.UI/Views/Controls/MainControl.cs
+++ DAMS.UI/Views/Controls/MainControl.cs
@@ -79,7 +79,7 @@ namespace DAMS.UI.Views.Controls
         {
             if (e.EventType == EventType.DeviceArrival)
             {
-                Thread.Sleep(5000);
+                Thread.Sleep(10000);
                 //检查当前设备是否需要续传文件
                 var myVID = e.Device.IdVendor;
                 var myPID = e.Device.IdProduct;
