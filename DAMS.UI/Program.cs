51eb2f3181c238e0f71985d5093842d9e22b360b
付启忠 <qzfu@haiyi-info.com>
2020-07-10 12:17:38 +0800

    增加线程互斥，控制程序启动限制个数

--- DAMS.UI/Program.cs
+++ DAMS.UI/Program.cs
@@ -4,11 +4,14 @@ using System.Windows.Forms;
 using DAMS.UI.Views;
 using DAMS.Models.Initialize;
 using DAMS.UI.Common;
+using DAMS.Common;
 
 namespace DAMS.UI
 {
     static class Program
     {
+        private static System.Threading.Mutex mutex;
+
         private static IndexForm indexForm;
         /// <summary>
         /// The main entry point for the application.
@@ -19,11 +22,22 @@ namespace DAMS.UI
             //DatabaseInitializer.Initialize();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            LoginForm loginForm = new LoginForm();//加载登录窗体
-            if (loginForm.ShowDialog() == DialogResult.OK)
+
+            mutex = new System.Threading.Mutex(true, "DAMSRUNONLY");
+            if (mutex.WaitOne(0, false))
             {
-                indexForm = new IndexForm();
-                Application.Run(indexForm);//如果登录成功则打开主窗体
+                LoginForm loginForm = new LoginForm();//加载登录窗体
+                if (loginForm.ShowDialog() == DialogResult.OK)
+                {
+                    indexForm = new IndexForm();
+                    Application.Run(indexForm);//如果登录成功则打开主窗体
+                }
+            }
+
+            else
+            {
+                MessageUtil.ShowMessage("程序已经在运行！", EnumData.MessageType.Warning);
+                Application.Exit();
             }
         }
     }
